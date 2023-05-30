05e87366c5924bd378d7f1cf2df98d8f70abe4d8
Lancelot-SO <fsowah001@gmail.com>
2023-05-30 07:56:06 +0000

    create models snd schema files

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    has_many :entities, dependent: :destroy
+    has_many :groups, dependent: :destroy
+  
+    validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+end
---
+++ db/migrate/20230530074745_create_groups.rb
@@ -0,0 +1,11 @@
+class CreateGroups < ActiveRecord::Migration[7.0]
+  def change
+    create_table :groups do |t|
+      t.string :name
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.string :icon
+
+      t.timestamps
+    end
+  end
+end
