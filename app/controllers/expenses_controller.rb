f14020937907b945a95c717f9fcdd7025f53d0fb
Lancelot-SO <fsowah001@gmail.com>
2023-05-30 08:09:16 +0000

    setup controllers

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,28 @@
 class ExpensesController < ApplicationController
+  def index
+    @group = Group.find(params[:group_id])
+    @expenses = @group.expenses.order('created_at DESC')
+  end
+
+  def new
+    @expense = current_user.expenses.new
+    @group = Group.find(params[:group_id])
+  end
+
+  def create
+    @expense = current_user.expenses.new(expense_params.slice(:name, :amount))
+    if @expense.save
+      @expense_group = ExpenseGroup.new(expense_id: @expense.id, group_id: params[:expense][:group_id])
+
+      redirect_to user_groups_path(current_user), notice: 'Expense created successfully' if @expense_group.save
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit!
+  end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,28 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!, except: [:splash]
+  def index
+    @groups = current_user.groups
+  end
+
+  def new
+    @group = current_user.groups.new
+  end
+
+  def create
+    @group = current_user.groups.new(group_params)
+    @group.icon.attach(params[:group][:icon])
+    if @group.save
+      redirect_to user_groups_path(current_user), notice: 'Catagories created successfully'
+    else
+      render :new
+    end
+  end
+
+  def splash; end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
