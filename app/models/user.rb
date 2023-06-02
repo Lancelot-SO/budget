2cb41d47837b1860949cfefd7622d552426ab314
Lancelot-SO <fsowah001@gmail.com>
2023-06-02 12:15:23 +0000

    solved changes requested

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true
   has_many :expenses, foreign_key: :author_id, dependent: :destroy
   has_many :groups, foreign_key: :author_id, dependent: :destroy
