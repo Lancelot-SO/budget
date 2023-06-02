a44d61301b786139cd366c5cbb6179f4018ddbe6
Lancelot-SO <fsowah001@gmail.com>
2023-06-02 13:17:37 +0000

    add test files (spec)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
   has_many :expenses, foreign_key: :author_id, dependent: :destroy
   has_many :groups, foreign_key: :author_id, dependent: :destroy
 end
