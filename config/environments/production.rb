828f65fb2b83b054a1b106c81e87b4fdd683211a
Lancelot-SO <fsowah001@gmail.com>
2023-06-02 08:11:25 +0000

    modify navbar

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
+  config.action_mailer.default_url_options = { host: 'https://budget-app-q1ts.onrender.com' }
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?  || ENV["RENDER"].present?
